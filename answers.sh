5eb64d055f2a30adafe2854d0cee99add02bc041
Sanya Vijay Gangwani <gangwans@data.cs.purdue.edu>
2022-02-04 18:04:51 -0500

    Done!

--- answers.sh
+++ answers.sh
@@ -3,47 +3,45 @@
 cd ORIGIN
 
 # Task 1 
-$ mv ~/HW2.txt ~/HW1.txt
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-$ rm -rf ~/Memes/SpongeBob/money.jpeg
+rm -rf Memes/SpongeBob/money.jpeg
 
 # Task 3
-$ mv ~/Memes/"Star Wars"/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
+mv Memes/"Star Wars"/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-$ rm -rf ~/Homework/CS191
+rm -rf Homework/CS191
 
 # Task 5
-$ mkdir Programs/C++
+mkdir Programs/C++
 
 # Task 6
-$ mv ~/Programs/Age.cpp ~/Programs/C++/Age.cpp
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-$ cp ~/Programs/Java ~/Homework/CS180/Project_1
+cp -r Programs/Java Homework/CS180/Project_1
 
 # Task 8
-$ mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-$ mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/"Python 3"
+mv Programs/Name.py Programs/Space.py Programs/"Python 3"
 
 # Task 10
-$ touch ~/Homework/POL141/Essay.txt
+touch Homework/POL141/Essay.txt
 
 # Task 11
-$ ls ..
+ls /homes/gangwans/homework-1-sanyagangwani
 
 # Task 12
-$ touch ~/.vimrc
-: set number
-: set showmatch
-: set autoread
+:
 
 Type your vim/nano customizations below:
  set number 
  set showmatch
- set autoread
+ set showmatch
+ set autoindent expandtab 
 
 
