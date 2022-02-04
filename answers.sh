730c78eb307ffea7ce332917c40ab923327af8a4
sanyagangwani <98429987+sanyagangwani@users.noreply.github.com>
2022-02-04 18:33:16 -0500

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -1,49 +1,45 @@
 #!/usr/bin/env bas
-# Changes to the ORIGIN directory
-cd ORIGIN
-
-# Task 1 
-$ mv ~/HW2.txt ~/HW1.txt
-
-# Task 2
-$ rm -rf ~/Memes/SpongeBob/money.jpeg
-
-# Task 3
-$ mv ~/Memes/"Star Wars"/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
-
-# Task 4
-$ rm -rf ~/Homework/CS191
-
-# Task 5
-$ mkdir Programs/C++
-
-# Task 6
-$ mv ~/Programs/Age.cpp ~/Programs/C++/Age.cpp
-
-# Task 7
-$ cp -r ~/Programs/Java ~/Homework/CS180/Project_1
-
-# Task 8
-$ mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
-
-# Task 9
-$ mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/"Python 3"
-
-# Task 10
-$ touch ~/Homework/POL141/Essay.txt
-
-# Task 11
-$ ls ..
-
-# Task 12
-$ touch ~/.vimrc
-: set number
-: set showmatch
-: set autoread
-
-Type your vim/nano customizations below:
- set number 
- set showmatch
- set autoread
-
-
+  2 # Changes to the ORIGIN directory
+  3 cd ORIGIN
+  4
+  5 # Task 1
+  6 mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
+  7
+  8 # Task 2
+  9 rm -rf Memes/SpongeBob/money.jpeg
+ 10
+ 11 # Task 3
+ 12 mv Memes/"Star Wars"/fafsa.jpg Memes/SpongeBob/fafsa.jpg
+ 13
+ 14 # Task 4
+ 15 rm -rf Homework/CS191
+ 16
+ 17 # Task 5
+ 18 mkdir Programs/C++
+ 19
+ 20 # Task 6
+ 21 mv Programs/Age.cpp Programs/C++/Age.cpp
+ 22
+ 23 # Task 7
+ 24 cp -r Programs/Java Homework/CS180/Project_1
+ 25
+ 26 # Task 8
+ 27 mv Programs/Count.c Programs/Goodbye.c Programs/C
+ 28
+ 29 # Task 9
+ 30 mv Programs/Name.py Programs/Space.py Programs/"Python 3"
+ 31
+ 32 # Task 10
+ 33 touch Homework/POL141/Essay.txt
+ 34
+ 35 # Task 11
+ 36 ls /homes/gangwans/homework-1-sanyagangwani
+ 37
+ 38 # Task 12
+ 39 :
+ 40
+ 41 Type your vim/nano customizations below:
+ 42  set number
+ 43  set showmatch
+ 44  set showmatch
+ 45  set autoindent expandtab
