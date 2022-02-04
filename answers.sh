35ace6fa98684c381f127e0cfa3fcddb9cf59a90
Sanya Vijay Gangwani <gangwans@data.cs.purdue.edu>
2022-02-04 15:32:02 -0500

    Done!

--- answers.sh
+++ answers.sh
@@ -1,45 +1,49 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env bas
 # Changes to the ORIGIN directory
 cd ORIGIN
 
 # Task 1 
-
+$ mv ~/HW2.txt ~/HW1.txt
 
 # Task 2
-
+$ rm -rf ~/Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+$ mv ~/Memes/"Star Wars"/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+$ rm -rf ~/Homework/CS191
 
 # Task 5
-
+$ mkdir Programs/C++
 
 # Task 6
-
+$ mv ~/Programs/Age.cpp ~/Programs/C++/Age.cpp
 
 # Task 7
-
+$ cp ~/Programs/Java ~/Homework/CS180/Project_1
 
 # Task 8
-
+$ mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
 
 # Task 9
-
+$ mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/"Python 3"
 
 # Task 10
-
+$ touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+$ ls ..
 
 # Task 12
-: '
+$ touch ~/.vimrc
+: set number
+: set showmatch
+: set autoread
+
 Type your vim/nano customizations below:
+ set number 
+ set showmatch
+ set autoread
 
 
-
-'
